b8c6652c7d0b06a1ea6e6552620dc21d233b7088
Ишка <you@example.com>
2026-03-04 19:59:47 +0300

    Namespace и name для URL

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'  # Добавляем namespace для приложения
+
 urlpatterns = [
-    path('', views.index),
-    # Добавляем путь для групп с slug-параметром
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),  # Добавляем name
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),  # Добавляем name
 ]
