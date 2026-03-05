70af980f1467e6ff67adcb23021766f4284a2e51
«Э» <>
2026-03-05 11:20:03 +0300

    Осуществление вывода информации из базы данных на страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
+from .models import Post
+
 def index(request):
-    """Главная страница Yatube"""
-    template = 'posts/index.html'
-    # Здесь в будущем будем передавать посты из базы данных
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title': 'Последние обновления на сайте',
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
     """Страница с постами, отфильтрованными по группам"""
